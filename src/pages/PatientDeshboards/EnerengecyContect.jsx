94b0516bf965930d7c94ec65dbf9a31701c0c7fd
Harsh Foujdar <harshbtp738@gmail.com>
2026-06-21 11:26:53 +0530

    dd

--- src/pages/PatientDeshboards/EnerengecyContect.jsx
+++ src/pages/PatientDeshboards/EnerengecyContect.jsx
@@ -2,8 +2,21 @@ import React from "react";
 import PatientSidebar from "./patientSidebar";
 import { PiAmbulanceFill } from "react-icons/pi";
 import { MdAddCall } from "react-icons/md";
-
+import { useEffect, useState } from "react";
 const EnerengecyContect = () => {
+  const [emergencyData, setEmergencyData] = React.useState([]);
+  useEffect(() => {
+    const fetchEmergencyData = async () => {
+      try {
+        const response = await fetch("http://localhost:5000/emergency/alldata");
+        const data = await response.json();
+        setEmergencyData(data.mobileNumbers);
+      } catch (error) {
+        console.error("Error fetching emergency data:", error);
+      }
+    };
+    fetchEmergencyData();
+  }, []);
   return (
     <>
       <PatientSidebar />
@@ -17,163 +30,30 @@ const EnerengecyContect = () => {
           </p>
         </div>
       </section>
-      <div className="grid grid-cols-1 gap-4 mt-8 mx-auto w-7xl">
-        <section className="flex justify-center items-center w-full mt-8 mx-auto ">
-          <div className="flex justify-evenly items-center bg-gray-200 w-full h-20  rounded-md shadow-md gap-50">
-            <div className="bg-blue-200 w-15 h-15 rounded-full flex justify-center items-center ">
-              <PiAmbulanceFill className="text-3xl text-blue-800" />
-            </div>
-            <div className="flex justify-center items-start gap-5">
-              <MdAddCall className="text-4xl text-green-500" />
-              <span className="text-lg font-medium">941425610</span>
-            </div>
-
-            <p className="text-lg font-medium">Manoj Kumar</p>
-
-            <button className="bg-blue-500 cursor-pointer text-white px-4 py-2 rounded-md hover:bg-blue-600">
-              Call Now
-            </button>
-          </div>
-        </section>
+      {emergencyData.map((item) => (
         <div className="grid grid-cols-1 gap-4 mt-8 mx-auto w-7xl">
-          <section className="flex justify-center items-center w-7xl  mt-8 mx-auto ">
+          <section className="flex justify-center items-center w-full mt-8 mx-auto ">
             <div className="flex justify-evenly items-center bg-gray-200 w-full h-20  rounded-md shadow-md gap-50">
               <div className="bg-blue-200 w-15 h-15 rounded-full flex justify-center items-center ">
                 <PiAmbulanceFill className="text-3xl text-blue-800" />
               </div>
               <div className="flex justify-center items-start gap-5">
                 <MdAddCall className="text-4xl text-green-500" />
-                <span className="text-lg font-medium">941425610</span>
+                <span className="text-lg font-medium">
+                  {item.mobile || "N/A"}
+                </span>
               </div>
-
-              <p className="text-lg font-medium">Manoj Kumar</p>
-
-              <button className="bg-blue-500 cursor-pointer text-white px-4 py-2 rounded-md hover:bg-blue-600">
+              <p className="text-lg font-medium">{item.name || "N/A"}</p>
+              <a
+                href={`tel:${item.mobile}`}
+                className="bg-blue-500 cursor-pointer text-white px-4 py-2 rounded-md hover:bg-blue-600"
+              >
                 Call Now
-              </button>
-            </div>
-          </section>
-          <section className="flex justify-center items-center w-7xl  mt-8 mx-auto ">
-            <div className="flex justify-evenly items-center bg-gray-200 w-full h-20  rounded-md shadow-md gap-50">
-              <div className="bg-blue-200 w-15 h-15 rounded-full flex justify-center items-center ">
-                <PiAmbulanceFill className="text-3xl text-blue-800" />
-              </div>
-              <div className="flex justify-center items-start gap-5">
-                <MdAddCall className="text-4xl text-green-500" />
-                <span className="text-lg font-medium">941425610</span>
-              </div>
-
-              <p className="text-lg font-medium">Manoj Kumar</p>
-
-              <button className="bg-blue-500 cursor-pointer text-white px-4 py-2 rounded-md hover:bg-blue-600">
-                Call Now
-              </button>
-            </div>
-          </section>
-          <section className="flex justify-center items-center w-7xl  mt-8 mx-auto ">
-            <div className="flex justify-evenly items-center bg-gray-200 w-full h-20  rounded-md shadow-md gap-50">
-              <div className="bg-blue-200 w-15 h-15 rounded-full flex justify-center items-center ">
-                <PiAmbulanceFill className="text-3xl text-blue-800" />
-              </div>
-              <div className="flex justify-center items-start gap-5">
-                <MdAddCall className="text-4xl text-green-500" />
-                <span className="text-lg font-medium">941425610</span>
-              </div>
-
-              <p className="text-lg font-medium">Manoj Kumar</p>
-
-              <button className="bg-blue-500 cursor-pointer text-white px-4 py-2 rounded-md hover:bg-blue-600">
-                Call Now
-              </button>
-            </div>
-          </section>
-          <section className="flex justify-center items-center w-7xl  mt-8 mx-auto ">
-            <div className="flex justify-evenly items-center bg-gray-200 w-full h-20  rounded-md shadow-md gap-50">
-              <div className="bg-blue-200 w-15 h-15 rounded-full flex justify-center items-center ">
-                <PiAmbulanceFill className="text-3xl text-blue-800" />
-              </div>
-              <div className="flex justify-center items-start gap-5">
-                <MdAddCall className="text-4xl text-green-500" />
-                <span className="text-lg font-medium">941425610</span>
-              </div>
-
-              <p className="text-lg font-medium">Manoj Kumar</p>
-
-              <button className="bg-blue-500 cursor-pointer text-white px-4 py-2 rounded-md hover:bg-blue-600">
-                Call Now
-              </button>
-            </div>
-          </section>
-          <section className="flex justify-center items-center w-7xl  mt-8 mx-auto ">
-            <div className="flex justify-evenly items-center bg-gray-200 w-full h-20  rounded-md shadow-md gap-50">
-              <div className="bg-blue-200 w-15 h-15 rounded-full flex justify-center items-center ">
-                <PiAmbulanceFill className="text-3xl text-blue-800" />
-              </div>
-              <div className="flex justify-center items-start gap-5">
-                <MdAddCall className="text-4xl text-green-500" />
-                <span className="text-lg font-medium">941425610</span>
-              </div>
-
-              <p className="text-lg font-medium">Manoj Kumar</p>
-
-              <button className="bg-blue-500 cursor-pointer text-white px-4 py-2 rounded-md hover:bg-blue-600">
-                Call Now
-              </button>
-            </div>
-          </section>
-          <section className="flex justify-center items-center w-7xl  mt-8 mx-auto ">
-            <div className="flex justify-evenly items-center bg-gray-200 w-full h-20  rounded-md shadow-md gap-50">
-              <div className="bg-blue-200 w-15 h-15 rounded-full flex justify-center items-center ">
-                <PiAmbulanceFill className="text-3xl text-blue-800" />
-              </div>
-              <div className="flex justify-center items-start gap-5">
-                <MdAddCall className="text-4xl text-green-500" />
-                <span className="text-lg font-medium">941425610</span>
-              </div>
-
-              <p className="text-lg font-medium">Manoj Kumar</p>
-
-              <button className="bg-blue-500 cursor-pointer text-white px-4 py-2 rounded-md hover:bg-blue-600">
-                Call Now
-              </button>
-            </div>
-          </section>
-          <section className="flex justify-center items-center w-7xl  mt-8 mx-auto ">
-            <div className="flex justify-evenly items-center bg-gray-200 w-full h-20  rounded-md shadow-md gap-50">
-              <div className="bg-blue-200 w-15 h-15 rounded-full flex justify-center items-center ">
-                <PiAmbulanceFill className="text-3xl text-blue-800" />
-              </div>
-              <div className="flex justify-center items-start gap-5">
-                <MdAddCall className="text-4xl text-green-500" />
-                <span className="text-lg font-medium">941425610</span>
-              </div>
-
-              <p className="text-lg font-medium">Manoj Kumar</p>
-
-              <button className="bg-blue-500 cursor-pointer text-white px-4 py-2 rounded-md hover:bg-blue-600">
-                Call Now
-              </button>
-            </div>
-          </section>
-          <section className="flex justify-center items-center w-7xl  mt-8 mx-auto ">
-            <div className="flex justify-evenly items-center bg-gray-200 w-full h-20  rounded-md shadow-md gap-50">
-              <div className="bg-blue-200 w-15 h-15 rounded-full flex justify-center items-center ">
-                <PiAmbulanceFill className="text-3xl text-blue-800" />
-              </div>
-              <div className="flex justify-center items-start gap-5">
-                <MdAddCall className="text-4xl text-green-500" />
-                <span className="text-lg font-medium">941425610</span>
-              </div>
-
-              <p className="text-lg font-medium">Manoj Kumar</p>
-
-              <button className="bg-blue-500 cursor-pointer text-white px-4 py-2 rounded-md hover:bg-blue-600">
-                Call Now
-              </button>
+              </a>
             </div>
           </section>
         </div>
-      </div>
+      ))}
     </>
   );
 };
